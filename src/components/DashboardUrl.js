fc0a8b4813d88f13baaf6cb1c65046bec6918e3f
Harsh Khare <harshkhare3@gmail.com>
2021-09-30 12:46:48 +0530

    Refactored Dashboard

--- src/components/DashboardUrl.js
+++ src/components/DashboardUrl.js
@@ -5,37 +5,32 @@ import { Button, Row, Col } from "react-bootstrap";
 const DashboardUrl = ({ fileUrl }) => {
   return (
     <>
-      {fileUrl && (
-        <>
-          <h3>Share this dashboard link with your client!</h3>
-          <br /> <br />
-          <Row>
-            <Col sm={11}>
-              <br />
-              <p>{fileUrl}</p>
-            </Col>
-            <Col sm={1}>
-              <Button
-                onClick={() => {
-                  navigator.clipboard.writeText(fileUrl);
-                }}
-                className="my-2"
-                color="green"
-                type="submit"
-                data-tip="Copy to Clipboard!"
-              >
-                Copy
-              </Button>
-            </Col>
-          </Row>
-          <ReactTooltip
-            place="top"
-            backgroundColor="#31C3ED"
-            textColor="white"
-            effect="float"
-          />
-        </>
-      )}
+      <h3>Share this dashboard link with your client!</h3>
+      <br /> <br />
+      <Row>
+        <Col sm={11}>
+          <br />
+          <p>{fileUrl}</p>
+        </Col>
+        <Col sm={1}>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(fileUrl);
+            }}
+            className="my-2"
+            type="submit"
+            data-tip="Copy to Clipboard!"
+          >
+            Copy
+          </Button>
+        </Col>
+      </Row>
+      <ReactTooltip
+        place="top"
+        backgroundColor="#31C3ED"
+        textColor="white"
+        effect="float"
+      />
     </>
   );
 };
