fc70c22f8184847b6b0ecfe1da57a78a713b1b8a
Harsh Khare <harshkhare3@gmail.com>
2021-09-30 12:46:18 +0530

    Refactored App and index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import DashboardUrl from "./components/DashboardUrl";
-import Login from "./components/Login";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [fileUrl, setFileUrl] = useState("");
@@ -12,9 +12,10 @@ const App = () => {
         className="d-flex flex-column justify-content-center"
         style={{ height: "100%" }}
       >
-        <Login setFileUrl={setFileUrl} />
+        <h1>Login Page</h1>
+        <LoginForm setFileUrl={setFileUrl} />
         <Row className="py-5 mt-4"></Row>
-        <DashboardUrl fileUrl={fileUrl} />
+        { fileUrl && <DashboardUrl fileUrl={fileUrl} /> }
       </Container>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import "./vendor/bootstrap.min.css";
 import App from "./App";
 import "./index.css";
+import dotenv from "dotenv";
+
+dotenv.config();
 ReactDOM.render(<App />, document.getElementById("root"));
