9e222b592a55e460d884e3b4b3d1b4fb9b75b8ee
David Milanese <milanese.david@gmail.com>
2021-10-01 21:32:22 +0930

    chore: housekeeping

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./vendor/bootstrap.min.css";
-import App from "./App";
-import "./index.css";
 import dotenv from "dotenv";
 
+import "./vendor/bootstrap.min.css";
+import "./index.css";
+
+import App from "./App";
+
 dotenv.config();
+
 ReactDOM.render(<App />, document.getElementById("root"));
