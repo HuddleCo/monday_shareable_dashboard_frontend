b2f0cac48aabd63065e8ffd2556fae61a5056ba0
David Milanese <milanese.david@gmail.com>
2022-02-05 21:59:05 +1030

    feat: configure web vitals

--- src/index.js
+++ src/index.js
@@ -5,5 +5,16 @@ import "./vendor/bootstrap.min.css";
 import "./index.css";
 
 import App from "./App";
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
